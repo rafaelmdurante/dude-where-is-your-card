612d55f59003ad861094fbfeddcf54af164f93bf
padomingos <pandre.domingos@gmail.com>
2019-06-09 19:12:14 +0100

    added Beer

--- src/org/academiadecodigo/whiledcards/dudewhereisyourcard/objects/Beer.java
+++ src/org/academiadecodigo/whiledcards/dudewhereisyourcard/objects/Beer.java
@@ -1,4 +1,15 @@
 package org.academiadecodigo.whiledcards.dudewhereisyourcard.objects;
 
-public class Beer {
+public class Beer extends GameObjects implements Catchable{
+    private boolean caught;
+
+    public boolean isCaught(){
+        return caught;
+    }
+
+    public void setCaught(){
+
+        caught = !caught;
+    }
+
 }
