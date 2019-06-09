4fde5c40f57b7640856dead1025cc0843ff8cebb
WM <plasticinegiga@gmail.com>
2019-06-09 19:09:42 +0100

    Person class done

--- src/org/academiadecodigo/whiledcards/dudewhereisyourcard/objects/characteres/Person.java
+++ src/org/academiadecodigo/whiledcards/dudewhereisyourcard/objects/characteres/Person.java
@@ -1,5 +1,6 @@
 package org.academiadecodigo.whiledcards.dudewhereisyourcard.objects.characteres;
 import org.academiadecodigo.whiledcards.dudewhereisyourcard.gfx.Direction;
+import org.academiadecodigo.whiledcards.dudewhereisyourcard.gfx.Grid;
 import org.academiadecodigo.whiledcards.dudewhereisyourcard.objects.GameObjects;
 
 public abstract class Person extends GameObjects {
@@ -8,8 +9,13 @@ public abstract class Person extends GameObjects {
     private Direction currentDirection;
 
 
+    /** constructor
+     *
+     * @param grid
+     */
+    public Person(Grid grid) {
+        super(grid);
+    }
 
-
-
-
+    public abstract void move(Direction direction);
 }
