e4d8bea8a561e8465a3a4e24efb53cb0dfec6280
padomingos <pandre.domingos@gmail.com>
2019-06-09 20:15:45 +0100

    added Friend

--- src/org/academiadecodigo/whiledcards/dudewhereisyourcard/objects/characteres/Friend.java
+++ src/org/academiadecodigo/whiledcards/dudewhereisyourcard/objects/characteres/Friend.java
@@ -1,4 +1,23 @@
 package org.academiadecodigo.whiledcards.dudewhereisyourcard.objects.characteres;
 
-public class Friend {
+import org.academiadecodigo.whiledcards.dudewhereisyourcard.gfx.DirectionRandomizable;
+import org.academiadecodigo.whiledcards.dudewhereisyourcard.objects.Catchable;
+import org.academiadecodigo.whiledcards.dudewhereisyourcard.objects.GameObjects;
+
+public class Friend extends GameObjects implements Catchable, DirectionRandomizable {
+    private boolean caught;
+
+    @Override
+    public boolean isCaught() {
+        return caught;
+    }
+
+    public void setCaught(boolean caught) {
+        this.caught = caught;
+    }
+
+    @Override
+    public void chooseDirection() {
+        
+    }
 }
