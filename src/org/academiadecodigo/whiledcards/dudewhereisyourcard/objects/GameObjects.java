c00051b830ac4c3b6dd7d5ccd7014d325159bdf7
WM <plasticinegiga@gmail.com>
2019-06-09 18:24:04 +0100

    added GameObjects

--- src/org/academiadecodigo/whiledcards/dudewhereisyourcard/objects/GameObjects.java
+++ src/org/academiadecodigo/whiledcards/dudewhereisyourcard/objects/GameObjects.java
@@ -1,4 +1,26 @@
 package org.academiadecodigo.whiledcards.dudewhereisyourcard.objects;
 
+import org.academiadecodigo.whiledcards.dudewhereisyourcard.gfx.Grid;
+import org.academiadecodigo.whiledcards.dudewhereisyourcard.gfx.Position;
+
 public abstract class GameObjects {
+
+    private Position position;
+    private Grid grid;
+
+
+    public Position getPosition(){
+        return position;
+    }
+
+
+    /** receives a grid as argument so all objects use this grid
+     *
+     * @param grid
+     */
+    public void setGrid(Grid grid){
+        this.grid = grid;
+    }
+
+
 }
