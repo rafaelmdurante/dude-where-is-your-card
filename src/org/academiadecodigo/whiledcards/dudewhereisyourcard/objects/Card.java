894335a93ed354542f2af971ff33a3dfdd6682db
rafaelmdurante <rafaelmdurante@gmail.com>
2019-06-09 18:50:47 +0100

    class card created

--- src/org/academiadecodigo/whiledcards/dudewhereisyourcard/objects/Card.java
+++ src/org/academiadecodigo/whiledcards/dudewhereisyourcard/objects/Card.java
@@ -1,4 +1,30 @@
 package org.academiadecodigo.whiledcards.dudewhereisyourcard.objects;
 
-public class Card {
+import org.academiadecodigo.whiledcards.dudewhereisyourcard.gfx.*;
+
+public class Card extends GameObjects implements Catchable {
+
+    // Attributes from SuperClass
+    private Position position;
+    private Grid grid;
+    // Card attributes
+    private boolean caught;
+
+    // Methods from SuperClass
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
+
+    // Methods from Interface
+    public boolean isCaught() {
+        return caught;
+    }
+
+    public void setCaught() {
+        caught = true;
+    }
 }
