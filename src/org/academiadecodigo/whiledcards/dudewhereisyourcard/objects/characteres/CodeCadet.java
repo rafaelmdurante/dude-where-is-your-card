5a413a0a4fe1b0d948cfda09809f4b074886314e
rafaelmdurante <rafaelmdurante@gmail.com>
2019-06-09 20:15:25 +0100

    class codecadet created | missing constructor and move method

--- src/org/academiadecodigo/whiledcards/dudewhereisyourcard/objects/characteres/CodeCadet.java
+++ src/org/academiadecodigo/whiledcards/dudewhereisyourcard/objects/characteres/CodeCadet.java
@@ -1,4 +1,47 @@
 package org.academiadecodigo.whiledcards.dudewhereisyourcard.objects.characteres;
 
-public class CodeCadet {
+import org.academiadecodigo.whiledcards.dudewhereisyourcard.objects.Catchable;
+
+public class CodeCadet extends Person implements Catchable {
+
+    // Game Objects Attributes: position, grid | Methods: getPosition, setGrid
+    // Person Attributes: speed,  currentDirection,  picture | Methods: move
+
+    // CodeCadet Attributes
+    private boolean drunk;
+    private boolean caught;
+    private int movementCounter;
+
+    // Constructor
+
+    // Methods
+    public void setDrunk(boolean value) {
+        drunk = value;
+    }
+
+    public boolean isDrunk() {
+        return drunk;
+    }
+
+    public int getMovementCounter() {
+        return movementCounter;
+    }
+
+    /**
+     * Used to reset movement counter to become sober again whenever needed
+     */
+    public void resetMovementCounter() {
+        movementCounter = 0;
+    }
+
+    // Catchable methods
+    @Override
+    public boolean isCaught() {
+        return caught;
+    }
+
+    @Override
+    public void setCaught() {
+        caught = true:
+    }
 }
