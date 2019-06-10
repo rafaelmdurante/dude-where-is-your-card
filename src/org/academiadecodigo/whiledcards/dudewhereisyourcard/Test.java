f4254e84fc40810ea3ef114d8ae12ff92703bec7
Francisco Caldeira <francisco.caldra@gmail.com>
2019-06-10 17:09:03 +0100

    added collision codecadet to beer

--- src/org/academiadecodigo/whiledcards/dudewhereisyourcard/Test.java
+++ src/org/academiadecodigo/whiledcards/dudewhereisyourcard/Test.java
@@ -3,6 +3,8 @@ package org.academiadecodigo.whiledcards.dudewhereisyourcard;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 import org.academiadecodigo.whiledcards.dudewhereisyourcard.gfx.Grid;
 import org.academiadecodigo.whiledcards.dudewhereisyourcard.gfx.Position;
+import org.academiadecodigo.whiledcards.dudewhereisyourcard.objects.Beer;
+import org.academiadecodigo.whiledcards.dudewhereisyourcard.objects.GameObject;
 import org.academiadecodigo.whiledcards.dudewhereisyourcard.objects.characteres.*;
 
 
@@ -12,17 +14,23 @@ public class Test {
         Grid grid = new Grid(25,15);
         grid.init();
 
-        Position pos = new Position(grid, "beer2.png");
-        pos.choosePicture();
-        pos.show();
+        Beer beer = new Beer(grid, "beer2.png");
         CodeCadet cadet = new CodeCadet(grid,"red.png",1);
-        cadet.move();
 
         Friend friend = new Friend(grid, "green.png", 1);
         Guard guard = new Guard(grid, "pink.png",2);
         guard.setTarget(friend.getPosition());
+
+        GameObject[] g = {guard, beer, cadet};
+
+        CollisionDetector detector = new CollisionDetector(g);
+        cadet.move();
+
         while(true) {
             Thread.sleep(150);
+
+
+            detector.isUnSafe();
             friend.move();
             guard.move();
         }
