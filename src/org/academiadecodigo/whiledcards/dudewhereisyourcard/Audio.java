c98459e0c0faf92add9cfe46fde3e82fbf0cb32a
WM <plasticinegiga@gmail.com>
2019-06-14 21:32:05 +0100

    file wav change name to song

--- src/org/academiadecodigo/whiledcards/dudewhereisyourcard/Audio.java
+++ src/org/academiadecodigo/whiledcards/dudewhereisyourcard/Audio.java
@@ -2,16 +2,14 @@ package org.academiadecodigo.whiledcards.dudewhereisyourcard;
 
 import javax.sound.sampled.*;
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.InputStream;
-import java.net.URL;
 
 public class Audio {
 
     public void runAudio() {
         try {
             ClassLoader classLoader = Audio.class.getClassLoader();
-            InputStream inputStream = classLoader.getResourceAsStream("background-song.wav");
+            InputStream inputStream = classLoader.getResourceAsStream("song.wav");
 
                 Clip clip = AudioSystem.getClip();
                 AudioInputStream ais = AudioSystem.getAudioInputStream(new BufferedInputStream(inputStream));
